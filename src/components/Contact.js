470a485e9133d0ca61163402514ad7ed2fcb866a
stmcf <stevenmcfarlane@hotmail.co.uk>
2021-02-16 12:04:10 +0000

    copied most of the content from previous website git repo into this one. Changed the styling and removed react router which was gratuitous.

---
+++ src/components/Contact.js
@@ -0,0 +1,11 @@
+function Contact() {
+    return (
+        
+            <section className="contact-container">                
+            <p>We are developing our site and software from our offices in Stirling,</p>
+                <p>To stay up to date with our development or if you're a SME and would like to discuss how we can help you please contact us using the form below, or via the following email address;</p>
+        </section>
+    );
+};
+
+export default Contact;
---
+++ src/components/Main.js
@@ -0,0 +1,15 @@
+import Contact from './Contact.js';
+import Detail from './Detail.js';
+
+function Main() {
+    return (
+        <main className="content">
+        <div>
+           <Detail/>
+           <Contact/>
+        </div>
+        </main>
+    );
+};
+
+export default Main;
