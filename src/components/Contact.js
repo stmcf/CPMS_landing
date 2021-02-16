2db2a538343c4e51e0aab60d1ccf60a019f55a08
stmcf <stevenmcfarlane@hotmail.co.uk>
2021-02-16 12:35:34 +0000

    styling changed and contact details added. router likely required now for contact us

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,9 +1,13 @@
+import '../styles/contact.css';
+
 function Contact() {
     return (
         
             <section className="contact-container">                
             <p>We are developing our site and software from our offices in Stirling,</p>
                 <p>To stay up to date with our development or if you're a SME and would like to discuss how we can help you please contact us using the form below, or via the following email address;</p>
+                <p>We are most active on LinkedIn however if you would like to discuss a project or specific requirements, please <a className='email-us' href="mailto:info@cpm-software.co.uk">Contact us</a>
+                    </p>
         </section>
     );
 };
--- src/components/Detail.js
+++ src/components/Detail.js
@@ -4,15 +4,19 @@ import '../styles/detail.css';
 function Detail() {
     return (
         <>
-        <div id="cpm-logo">
-                <img src='./CPMLogo.png' alt="CPM Software logo" className="cpm-logo"></img>
-                </div>
-        <h1 className="name">
-				<span>C</span>PM
-			</h1>
+        <div className="header">
             <h1 className="name">
-				&nbsp;&nbsp;&nbsp;<span>S</span>oftware<span>L</span>imited
+				<span>C</span>PM&nbsp;&nbsp;&nbsp;<span>S</span>oftware&nbsp;&nbsp;&nbsp;<span>L</span>imited
 			</h1>
+            {/* <h1 className="name">
+				
+			</h1> */}
+            </div>
+            <div id="cpm-logo">
+                <img src='./CPMLogo.png' alt="CPM Software logo" className="cpm-logo"></img>
+            </div>
+
+
 			<div className="role-container">
 				<p className="blurb"><span className="blueText">Cost Management Software</span> <br/> for <span className="blueText">SME's</span> <br/> based in Stirling, Scotland.</p>
 			</div>
