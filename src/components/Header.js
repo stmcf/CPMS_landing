4c90250d609e7ac69c4c65646424911988369aad
stmcf <stevenmcfarlane@hotmail.co.uk>
2021-03-10 08:56:54 +0000

    minor changes to header. Not responsive for mobiles - next task is to re-work for smaller screens.

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,9 +3,9 @@ import '../styles/header.css';
 function Header() {
     return (
         <div className="header">
-            <div id="cpm-logo">
+            {/* <div id="cpm-logo"> */}
                 <img src='./CPMLogo.png' alt="CPM Software logo" className="cpm-logo"></img>
-            </div>
+            {/* </div> */}
             <div className="header-contact">
                 <a className='header-email-us' href="mailto:info@cpm-software.co.uk">Contact us</a>
             </div>
